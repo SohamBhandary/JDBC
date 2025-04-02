47f44608098b5cf08b63b46a731fee9a24806167
SohamBhandary <bhandarysoham27@gmail.com>
2025-04-02 20:10:53 +0530

    Execute and process

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -14,11 +14,22 @@ public class DemoJdbc {
         String url="jdbc:postgresql://localhost:5432/Demo";
         String uname="postgres";
         String pass="0000";
+        String sql="select sname from student where sid=1";
 //        Class.forName("org.postgresql.Driver");
 
         Connection con = DriverManager.getConnection(url,uname,pass);
         System.out.println("connection established");
+        Statement st =con.createStatement();
+       ResultSet rs=  st.executeQuery(sql);
+        rs.next();
+       String name= rs.getString("sname");
+        System.out.println("Name is :"+ name);
+
         con.close();
+        System.out.println("connection closed");
+
+
+
 
         }
     }
